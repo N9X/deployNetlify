6b7f1acc6c686dc81c688ea896c7391d140d272f
dev-patel27 <dev4522patel@gmail.com>
2022-09-10 18:25:13 +0530

    deploy

---
+++ about-us/preload.637984112625690000.js
@@ -0,0 +1,3 @@
+export default function(preload){
+            return preload(()=> import('./pre.637984112625690000.js'),["assets/plugin-vue_export-helper.637984112625690000.js","assets/aboutus.component.637984112625690000.js","assets/investor.component.637984112625690000.css","assets/investor.component.637984112625690000.js"]);
+    }
